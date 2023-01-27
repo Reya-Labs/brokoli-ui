e565eb9211b7980bfc39745c9076c0e0513cd977
ssbarbee <ssbarbee@users.noreply.github.com>
2023-01-27 20:53:06 +0100

    test: fix broken Button test

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -5,7 +5,7 @@ import { render, screen } from '@testing-library/react';
 import { Button } from './index';
 
 describe('<Button />', () => {
-  it('Check Button Disabled', () => {
+  it('should render Button correctly', () => {
     render(<Button>Button</Button>);
     expect(screen.getByRole('button', { name: 'Button' }));
   });
