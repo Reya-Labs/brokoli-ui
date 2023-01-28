96569457ba53d1297d8d9764a486962cae76c48f
ssbarbee <ssbarbee@users.noreply.github.com>
2023-01-28 11:56:04 +0100

    fix: improve bundlesize and fix fonts import path

--- rollup.config.js
+++ rollup.config.js
@@ -1,24 +1,26 @@
-import commonjs from "@rollup/plugin-commonjs";
-import resolve from "@rollup/plugin-node-resolve";
-import typescript from "@rollup/plugin-typescript";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import { terser } from "rollup-plugin-terser";
-const dts = require('rollup-plugin-dts')
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
+import bundleSize from 'rollup-plugin-bundle-size';
+import copy from 'rollup-plugin-copy';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import { terser } from 'rollup-plugin-terser';
+const dts = require('rollup-plugin-dts');
 
-const packageJson = require("./package.json");
+const packageJson = require('./package.json');
 
 export default [
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: [
       {
         file: packageJson.main,
-        format: "cjs",
+        format: 'cjs',
         sourcemap: true,
       },
       {
         file: packageJson.module,
-        format: "esm",
+        format: 'esm',
         sourcemap: true,
       },
     ],
@@ -26,14 +28,22 @@ export default [
       peerDepsExternal(),
       resolve(),
       commonjs(),
-      typescript({ tsconfig: "./tsconfig.build.json" }),
+      typescript({ tsconfig: './tsconfig.build.json' }),
       terser(),
+      copy({
+        targets: [
+          // Need to copy the files over for usage otherwise library gets bloated
+          { src: 'src/foundation/Fonts/assets/*', dest: 'dist/cjs/assets/' },
+          { src: 'src/foundation/Fonts/assets/*', dest: 'dist/esm/assets/' },
+        ],
+      }),
+      bundleSize(),
     ],
-    external: ["react", "react-dom", "styled-components"],
+    external: ['react', 'react-dom', 'styled-components'],
   },
   {
-    input: "dist/esm/types/index.d.ts",
-    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    input: 'dist/esm/types/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [dts.default()],
   },
 ];
