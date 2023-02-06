a5de61b3216cb04a09474b562425d7e49c32e362
ssbarbee <ssbarbee@users.noreply.github.com>
2023-02-06 14:37:49 +0100

    chore: improve storybook theme

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,6 +1,7 @@
 import { addons } from '@storybook/addons';
+import darkTheme from './themes/dark';
 import { themes } from '@storybook/theming';
-
+console.log(themes.dark);
 addons.setConfig({
-  theme: themes.dark,
+  theme: darkTheme,
 });
---
+++ .storybook/themes/dark.js
@@ -0,0 +1,38 @@
+import { create } from '@storybook/theming';
+import { colors } from './colors';
+
+export default create({
+  base: 'dark',
+  colorPrimary: colors.wildStrawberry,
+  colorSecondary: colors.liberty4,
+
+  // UI
+  appBg: colors.liberty6,
+  appContentBg: colors.liberty7,
+  appBorderColor: colors.lavenderWeb6,
+  appBorderRadius: 4,
+
+  // Typography
+  fontBase: '"Inter", sans-serif',
+  fontCode: 'monospace',
+
+  // Text colors
+  textColor: colors.lavenderWeb,
+  textInverseColor: colors.lavenderWeb8,
+
+  // Toolbar default and active colors
+  barTextColor: colors.lavenderWeb,
+  barSelectedColor: colors.wildStrawberry,
+  barBg: colors.liberty6,
+
+  // Form colors
+  inputBg: colors.liberty5,
+  inputBorder: colors.lavenderWeb6,
+  inputTextColor: colors.lavenderWeb,
+  inputBorderRadius: 4,
+
+  brandTitle: 'brokoli-ui',
+  brandUrl: 'https://brokoli.voltz.xyz/',
+  brandImage: 'https://cdn.discordapp.com/emojis/938848589499732058.webp?size=128&quality=lossless',
+  brandTarget: '_self',
+});
