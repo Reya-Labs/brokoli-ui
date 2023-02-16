8d8b9f81bc15b907939068afc442c97a75de7b33
ssbarbee <ssbarbee@users.noreply.github.com>
2023-02-16 10:47:44 +0100

    fix: remove copy plugin

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,6 @@ import resolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import svgr from '@svgr/rollup';
 import bundleSize from 'rollup-plugin-bundle-size';
-import copy from 'rollup-plugin-copy';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import { terser } from 'rollup-plugin-terser';
 const dts = require('rollup-plugin-dts');
@@ -31,14 +30,6 @@ export default [
       commonjs(),
       typescript({ tsconfig: './tsconfig.build.json' }),
       terser(),
-      copy({
-        targets: [
-          // Need to copy the files over for usage otherwise library gets bloated
-          { src: 'src/foundation/Fonts/assets/*', dest: 'dist/cjs/assets/' },
-          { src: 'src/foundation/Fonts/assets/*', dest: 'dist/esm/assets/' },
-        ],
-      }),
-      svgr(),
       bundleSize(),
     ],
     external: ['react', 'react-dom', 'styled-components'],
