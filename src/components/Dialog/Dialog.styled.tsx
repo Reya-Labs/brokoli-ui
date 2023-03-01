9335ab5c82f24fc1e8971ae3c995d7d2ea9f32e0
ssbarbee <ssbarbee@users.noreply.github.com>
2023-03-01 14:09:25 +0100

    fix: set fixed width for Dialog

--- src/components/Dialog/Dialog.styled.tsx
+++ src/components/Dialog/Dialog.styled.tsx
@@ -14,4 +14,5 @@ export const DialogBox = styled('div')`
   box-shadow: 0px 4px 30px ${colors.liberty5}, 0px 0px 1px ${colors.lavenderWeb3},
     inset 1px 2px 6px ${colors.liberty8};
   border-radius: 4px;
+  width: 416px;
 `;
