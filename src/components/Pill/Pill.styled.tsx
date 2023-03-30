601bb8f93178cb884d18fb2aac6a6aca2fa385fc
ssbarbee <ssbarbee@users.noreply.github.com>
2023-03-30 12:05:29 +0200

    feat: support Rainbow for pill

--- src/components/Pill/Pill.styled.tsx
+++ src/components/Pill/Pill.styled.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 
+import { colors } from '../../foundation/Colors';
 import { Typography } from '../Typography';
 
 export const PillTypography = styled(Typography, {
@@ -10,4 +11,26 @@ export const PillTypography = styled(Typography, {
 
   background: ${({ backgroundColor }) => backgroundColor};
   border-radius: 4px;
+`;
+
+export const RainbowTypography = styled(Typography)`
+  display: inline-block;
+  background: linear-gradient(
+    90deg,
+    ${colors.wildStrawberry} 0%,
+    ${colors.orangeYellow} 31.47%,
+    ${colors.skyBlueCrayola} 68.91%,
+    ${colors.ultramarineBlue} 100%
+  );
+  background-clip: text;
+  text-fill-color: transparent;
+`;
+
+export const RainbowBox = styled('div', {
+  shouldForwardProp: (prop) => prop !== 'backgroundColor',
+})<{ backgroundColor: string }>`
+  display: inline-block;
+  padding: 4px 8px;
+  border-radius: 4px;
+  background: ${({ backgroundColor }) => backgroundColor};
 `;
--- src/components/Pill/index.tsx
+++ src/components/Pill/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import { BaseColorTokens, getColorFromToken } from '../../foundation/Colors';
+import { isBaseColorToken } from '../../foundation/Colors/isBaseColorToken';
 import { TypographyToken } from '../Typography';
-import { PillTypography } from './Pill.styled';
+import { PillTypography, RainbowBox, RainbowTypography } from './Pill.styled';
 
 export type PillProps = {
   children: string;
   className?: string;
-  colorToken: BaseColorTokens;
+  colorToken: BaseColorTokens | 'rainbow';
   typographyToken: TypographyToken;
   'data-testid'?: string;
 };
@@ -18,14 +19,32 @@ export const Pill = ({
   children,
   colorToken,
   className,
-}: PillProps) => (
-  <PillTypography
-    backgroundColor={getColorFromToken(`${colorToken}6`)}
-    className={className}
-    colorToken={colorToken}
-    data-testid={dataTestId || `Pill-PillTypography-${colorToken}-${typographyToken}`}
-    typographyToken={typographyToken}
-  >
-    {children}
-  </PillTypography>
-);
+}: PillProps) => {
+  const baseColorToken = isBaseColorToken(colorToken);
+  if (baseColorToken) {
+    return (
+      <PillTypography
+        backgroundColor={getColorFromToken(`${colorToken}6`)}
+        className={className}
+        colorToken={colorToken}
+        data-testid={dataTestId || `Pill-PillTypography-${colorToken}-${typographyToken}`}
+        typographyToken={typographyToken}
+      >
+        {children}
+      </PillTypography>
+    );
+  }
+
+  return (
+    <RainbowBox backgroundColor={getColorFromToken('liberty7')}>
+      <RainbowTypography
+        className={className}
+        colorToken="liberty7"
+        data-testid={dataTestId || `Pill-PillTypography-${colorToken}-${typographyToken}`}
+        typographyToken={typographyToken}
+      >
+        {children}
+      </RainbowTypography>
+    </RainbowBox>
+  );
+};
