9656d9e60bf8a13c1b995799daf72346623fba2f
ssbarbee <ssbarbee@users.noreply.github.com>
2023-05-16 15:27:05 +0200

    test: add Page.test.tsx

---
+++ src/components/Page/Page.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+
+import { Page } from '.';
+
+describe('<Page />', () => {
+  it('renders children and required elements', () => {
+    render(
+      <Page>
+        <div>Page Content</div>
+      </Page>,
+    );
+    expect(screen.getByTestId('Page-PageBox')).toBeInTheDocument();
+    expect(screen.getByTestId('Page-PageContentBox')).toBeInTheDocument();
+    expect(screen.getByTestId('Page-BackgroundBox')).toBeInTheDocument();
+    expect(screen.getByTestId('Page-BrandLogoBox1')).toBeInTheDocument();
+    expect(screen.getByTestId('Page-BrandLogoBox2')).toBeInTheDocument();
+    expect(screen.getByTestId('Page-BrandLogoBox3')).toBeInTheDocument();
+    expect(screen.getByTestId('Page-BackgroundNoiseBox')).toBeInTheDocument();
+    expect(screen.getByText('Page Content')).toBeInTheDocument();
+  });
+});
