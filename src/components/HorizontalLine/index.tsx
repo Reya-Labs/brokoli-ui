abd9dc5cbbf34b309a3e19a1e3172c3f7e51c846
ssbarbee <ssbarbee@users.noreply.github.com>
2023-05-18 22:18:58 +0200

    fix: line color is lavenderWeb7

--- src/components/HorizontalLine/index.tsx
+++ src/components/HorizontalLine/index.tsx
@@ -5,5 +5,5 @@ import { colors } from '../../foundation/Colors';
 export const HorizontalLine = styled('div')`
   box-sizing: border-box;
   width: 100%;
-  border: 0.5px solid ${colors.lavenderWeb5};
+  border: 0.5px solid ${colors.lavenderWeb7};
 `;
