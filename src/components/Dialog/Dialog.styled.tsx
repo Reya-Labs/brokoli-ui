508d2abde19c0e5691fb20256a2636d3dbc8f24f
ssbarbee <ssbarbee@users.noreply.github.com>
2023-05-18 21:41:24 +0200

    fix: improve styles of Dialog

--- src/components/Dialog/Dialog.styled.tsx
+++ src/components/Dialog/Dialog.styled.tsx
@@ -6,13 +6,11 @@ export const DialogBox = styled('div')`
   display: flex;
   flex-direction: column;
   align-items: center;
-  padding: 24px 40px;
+  padding: 16px;
 
   position: relative;
 
-  background: ${colors.liberty8};
-  box-shadow: 0px 4px 30px ${colors.liberty5}, 0px 0px 1px ${colors.lavenderWeb3},
-    inset 1px 2px 6px ${colors.liberty8};
-  border-radius: 4px;
-  width: 416px;
+  background: linear-gradient(90.95deg, ${colors.lavenderWeb8} 0.66%, ${colors.liberty8} 99.34%);
+  border: 1px solid ${colors.lavenderWeb7};
+  border-radius: 8px;
 `;
