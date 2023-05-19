2d5f1ece627672a91f176120eb6b465499ada082
ssbarbee <ssbarbee@users.noreply.github.com>
2023-05-19 11:18:38 +0200

    fix: add boxshadow to Dialog

--- src/components/Dialog/Dialog.styled.tsx
+++ src/components/Dialog/Dialog.styled.tsx
@@ -9,7 +9,7 @@ export const DialogBox = styled('div')`
   padding: 16px;
 
   position: relative;
-
+  box-shadow: 0px 0px 40px 0px rgba(225, 221, 247, 0.1);
   background: linear-gradient(90.95deg, ${colors.lavenderWeb8} 0.66%, ${colors.liberty8} 99.34%);
   border: 1px solid ${colors.lavenderWeb7};
   border-radius: 8px;
