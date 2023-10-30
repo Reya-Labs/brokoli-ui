0b5787c2679a106dea4d9979424d94698d4398da
ssbarbee <ssbarbee@users.noreply.github.com>
2023-10-30 16:39:15 +0100

    refactor: add Media

---
+++ src/foundation/Media/constants.ts
@@ -0,0 +1,8 @@
+import { SupportedMedias } from './types';
+
+export const mediaBreakPoints: Record<SupportedMedias, number> = {
+  largeDesktopDevice: 1920,
+  mobileDevice: 390,
+  smallDesktopDevice: 1440,
+  tabletDevice: 834,
+};
---
+++ src/foundation/Media/types.ts
@@ -0,0 +1,5 @@
+export type SupportedMedias =
+  | 'mobileDevice'
+  | 'tabletDevice'
+  | 'smallDesktopDevice'
+  | 'largeDesktopDevice';
