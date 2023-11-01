0c2bfad9f0bc8b0a083f62832bb3b41db87ec300
ssbarbee <ssbarbee@users.noreply.github.com>
2023-11-01 13:55:00 +0100

    chore: format

--- jest.config.js
+++ jest.config.js
@@ -4,18 +4,18 @@
  */
 module.exports = {
   collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageReporters: ['lcov', 'json-summary'],
   collectCoverageFrom: [
     'src/**/*.{js,jsx,ts,tsx}',
     '!src/**/*.d.ts',
     '!src/**/*.stories.{js,jsx,ts,tsx}',
   ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['lcov', 'json-summary'],
+  setupFilesAfterEnv: ['./config/jest/setupTests.js'],
   testEnvironment: 'jsdom',
   transform: {
+    '^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)': '<rootDir>/config/jest/fileTransform.js',
     '^.+\\.(t|j)sx?$': ['@swc/jest'],
     '^.+\\.css$': '<rootDir>/config/jest/cssTransform.js',
-    '^(?!.*\\.(js|jsx|mjs|cjs|ts|tsx|css|json)$)': '<rootDir>/config/jest/fileTransform.js',
   },
-  setupFilesAfterEnv: ['./config/jest/setupTests.js'],
 };
