30d1724476d89e9c2aa5ed348cb93e7f9e106784
ssbarbee <ssbarbee@users.noreply.github.com>
2024-02-18 12:36:24 +0100

    fix: style

--- src/components/Popover/Popover.styled.tsx
+++ src/components/Popover/Popover.styled.tsx
@@ -1,15 +1,12 @@
 import { css, Theme } from '@emotion/react';
 
-import { addAlpha } from '../../foundation/Colors';
-
 export const TINY_POPOVER_CONTAINER_CLASS_NAME = 'react-tiny-popover-container';
 export const globalReactTinyPopoverContainerCSS = (theme: Theme) => css`
   .${TINY_POPOVER_CONTAINER_CLASS_NAME} {
-    background: ${theme.colors.black800};
+    background: ${theme.colors.black900};
 
-    border: 1px solid ${theme.colors.white800};
-    box-shadow: 0px 0px 20px ${addAlpha(theme.colors.white100, 0.1)};
-    border-radius: 4px;
+    border: 1px solid ${theme.colors.black500};
+    border-radius: 8px;
     min-width: 160px;
     margin-top: 4px;
     z-index: ${theme.layerIndexes.popover};
