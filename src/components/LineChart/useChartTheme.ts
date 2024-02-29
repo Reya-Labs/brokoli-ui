a3548c1175ed51fd577ff87db6b657f0a0904e51
ssbarbee <ssbarbee@users.noreply.github.com>
2024-02-29 17:41:41 +0100

    refactor: extract hook

--- src/components/LineChart/useChartTheme.ts
+++ src/components/LineChart/useChartTheme.ts
@@ -1,5 +1,5 @@
 import { useTheme } from '@emotion/react';
-import { Property } from 'csstype';
+import { useMemo } from 'react';
 
 import { getColorFromToken } from '../../foundation/Colors';
 import { useResponsiveQuery } from '../../foundation/Media';
@@ -19,60 +19,72 @@ export const useChartTheme = ({
   | 'axisTicksTextColorToken'
 >) => {
   const theme = useTheme();
-  const axisTypographyConfig = getTypographyFromToken({
-    theme,
-    token: axisTypographyToken,
-  });
   const { isSmallDesktopDeviceAndUp, isTabletDeviceAndUp, isMobileDeviceAndUp } =
     useResponsiveQuery();
-  const axisTypography = isMobileDeviceAndUp
-    ? axisTypographyConfig.mobileDevice
-    : isTabletDeviceAndUp
-    ? axisTypographyConfig.tabletDevice
-    : isSmallDesktopDeviceAndUp
-    ? axisTypographyConfig.smallDesktopDevice
-    : axisTypographyConfig.largeDesktopDevice;
-  const axisFontSize = parseInt(axisTypography.fontSize, 10);
-  const axisFontFamily = axisTypography.fontFamily;
-  const crossHairColor = getColorFromToken({ colorToken: crosshairColorToken, theme });
-  const axisTicksTextColor = getColorFromToken({ colorToken: axisTicksTextColorToken, theme });
-  const axisDomainLineColor =
-    axisDomainLineColorToken === 'transparent'
-      ? 'transparent'
-      : getColorFromToken({ colorToken: axisDomainLineColorToken, theme });
 
-  return {
-    axis: {
-      domain: {
+  return useMemo(() => {
+    const axisTypographyConfig = getTypographyFromToken({
+      theme,
+      token: axisTypographyToken,
+    });
+    const axisTypography = isMobileDeviceAndUp
+      ? axisTypographyConfig.mobileDevice
+      : isTabletDeviceAndUp
+      ? axisTypographyConfig.tabletDevice
+      : isSmallDesktopDeviceAndUp
+      ? axisTypographyConfig.smallDesktopDevice
+      : axisTypographyConfig.largeDesktopDevice;
+    const axisFontSize = parseInt(axisTypography.fontSize, 10);
+    const axisFontFamily = axisTypography.fontFamily;
+    const crossHairColor = getColorFromToken({ colorToken: crosshairColorToken, theme });
+    const axisTicksTextColor = getColorFromToken({ colorToken: axisTicksTextColorToken, theme });
+    const axisDomainLineColor =
+      axisDomainLineColorToken === 'transparent'
+        ? 'transparent'
+        : getColorFromToken({ colorToken: axisDomainLineColorToken, theme });
+
+    return {
+      axis: {
+        domain: {
+          line: {
+            stroke: axisDomainLineColor,
+            strokeWidth: 1,
+          },
+        },
+        ticks: {
+          line: {
+            stroke: theme.colors.white400,
+            strokeWidth: 1,
+          },
+          text: {
+            fill: axisTicksTextColor,
+            fontFamily: axisFontFamily,
+            fontSize: axisFontSize,
+            fontWeight: parseInt(axisTypography.fontWeight),
+          },
+        },
+      },
+      background: 'transparent',
+      crosshair: {
         line: {
-          stroke: axisDomainLineColor,
+          stroke: crossHairColor,
           strokeWidth: 1,
         },
       },
-      ticks: {
-        line: {
-          stroke: theme.colors.white400,
-          strokeWidth: 1,
-        },
-        text: {
-          fill: axisTicksTextColor,
-          fontFamily: axisFontFamily as Property.FontFamily,
-          fontSize: axisFontSize,
-          fontWeight: parseInt(axisTypography.fontWeight),
-        },
+      text: {
+        color: theme.colors.white400,
+        fontFamily: axisFontFamily,
+        fontSize: axisFontSize,
       },
-    },
-    background: 'transparent',
-    crosshair: {
-      line: {
-        stroke: crossHairColor,
-        strokeWidth: 1,
-      },
-    },
-    text: {
-      color: theme.colors.white400,
-      fontFamily: axisFontFamily as Property.FontFamily,
-      fontSize: axisFontSize,
-    },
-  };
+    };
+  }, [
+    theme,
+    crosshairColorToken,
+    axisTicksTextColorToken,
+    axisDomainLineColorToken,
+    axisTypographyToken,
+    isSmallDesktopDeviceAndUp,
+    isTabletDeviceAndUp,
+    isMobileDeviceAndUp,
+  ]);
 };
