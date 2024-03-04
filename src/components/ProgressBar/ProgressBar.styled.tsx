42fa52535ff7110e550cba038e62a01567d23dfb
Antonio <antoniosimeonov7@gmail.com>
2024-03-04 10:24:52 +0200

    fix: change background color of progressbar

--- src/components/ProgressBar/ProgressBar.styled.tsx
+++ src/components/ProgressBar/ProgressBar.styled.tsx
@@ -19,6 +19,6 @@ export const ProgressBarPercentageBox = styled('div')<{ percentage: number }>`
 
 export const ProgressBarBoxContainer = styled('div')`
   width: 100%;
-  background: ${({ theme }) => theme.colors.white400};
+  background: ${({ theme }) => theme.colors.black700};
   height: 4px;
 `;
