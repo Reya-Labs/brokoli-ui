c7556df297b9f6f0e8cac0c0eb3199a370d0bc2c
ssbarbee <ssbarbee@users.noreply.github.com>
2024-03-20 17:28:10 +0100

    docs: comments

--- src/foundation/Media/types.ts
+++ src/foundation/Media/types.ts
@@ -16,8 +16,17 @@ export type SupportedMedias =
   | 'desktopDeviceOnly';
 
 export type SupportedMediasWidths =
+  // 0 -> mobileDevice.breakPoint - 1
   | 'mobileDevice'
+
+  // mobileDevice.breakPoint -> tabletDevice.breakPoint - 1
   | 'tabletDevice'
+
+  // tabletDevice.breakPoint -> smallDesktopDevice.breakPoint - 1
   | 'smallDesktopDevice'
+
+  // smallDesktopDevice.breakPoint -> desktopDevice.breakPoint - 1
   | 'desktopDevice'
+
+  // desktopDevice.breakPoint -> Infinity
   | 'largeDesktopDevice';
