5d163e8e032759d7064dda028a031fdba58bfa01
Antonio <antoniosimeonov7@gmail.com>
2024-03-22 11:06:46 +0200

    fix: custom hook logic

--- src/utils/hooks/useScrollDetector/index.ts
+++ src/utils/hooks/useScrollDetector/index.ts
@@ -2,22 +2,16 @@ import { useEffect, useState } from 'react';
 
 export const useScrollDetector = () => {
   const [hasScrolled, setHasScrolled] = useState(false);
-  const [lastScrollTop, setLastScrollTop] = useState(0);
 
   useEffect(() => {
     const handleScroll = () => {
       const currentScrollTop = window.scrollY || document.documentElement.scrollTop;
-      if (currentScrollTop === 0) {
-        setHasScrolled(false);
-      } else {
-        setHasScrolled(true);
-      }
-      setLastScrollTop(currentScrollTop);
+      setHasScrolled(currentScrollTop !== 0);
     };
 
     window.addEventListener('scroll', handleScroll);
     return () => window.removeEventListener('scroll', handleScroll);
-  }, [lastScrollTop]);
+  }, []);
 
   return { hasScrolled };
 };
