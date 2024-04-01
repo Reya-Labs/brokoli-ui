1512dfc8c1dd01081ec037955e2ce4a74682d72b
ssbarbee <ssbarbee@users.noreply.github.com>
2024-04-01 18:25:00 +0200

    feat: add value and valueFormatter

--- src/components/RangeField/Thumb/index.tsx
+++ src/components/RangeField/Thumb/index.tsx
@@ -2,22 +2,27 @@ import React from 'react';
 import { ReactSliderProps } from 'react-slider';
 
 import { ColorTokens } from '../../../foundation/Colors';
-import { StyledThumb } from './Thumb.styled';
+import { StyledThumb, StyledValueTypography } from './Thumb.styled';
 
 export type ThumbProps = {
   size: number;
   trackHeight: number;
   colorToken: ColorTokens;
+  valueFormatter: (value: number) => string;
 };
 
 export const Thumb: (thumbProps: ThumbProps) => ReactSliderProps['renderThumb'] =
-  ({ trackHeight, size, colorToken }) =>
-  (props) => (
+  ({ valueFormatter, trackHeight, size, colorToken }) =>
+  (props, state) => (
     <StyledThumb
       {...(props as object)}
       colorToken={colorToken}
       height={size}
       trackHeight={trackHeight}
       width={size}
-    />
+    >
+      <StyledValueTypography colorToken="white100" typographyToken="bodyXSmallRegular">
+        {valueFormatter(state.valueNow)}
+      </StyledValueTypography>
+    </StyledThumb>
   );
