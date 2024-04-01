7be55e59b0505cac81500fdecdbdb17a82f19ea1
ssbarbee <ssbarbee@users.noreply.github.com>
2024-04-01 14:49:59 +0200

    feat: initial commit

--- src/components/RangeField/index.tsx
+++ src/components/RangeField/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { ReactSliderProps } from 'react-slider';
 
 import { ColorTokens } from '../../foundation/Colors';
-import { ProgressBarProps } from '../ProgressBar';
-import { BoxContainer, Segment, SegmentBox, StyledInput } from './RangeField.styled';
+import {
+  BoxContainer,
+  StyledMark,
+  StyledSlider,
+  StyledThumb,
+  StyledTrack,
+} from './RangeField.styled';
 
-type RangeFieldProps = ProgressBarProps & {
+type RangeFieldProps = {
   trackColorToken?: 'transparent' | ColorTokens;
   thumbColorToken: ColorTokens;
   value?: number;
@@ -16,9 +22,20 @@ type RangeFieldProps = ProgressBarProps & {
   segmentColorToken?: ColorTokens;
 };
 
+const Thumb: ReactSliderProps['renderThumb'] = (props) => (
+  <StyledThumb size={6} thumbColorToken="white100" {...(props as object)} />
+);
+
+const Track: ReactSliderProps['renderTrack'] = (props, state) => (
+  <StyledTrack trackColorToken="white100" {...(props as object)} index={state.index} />
+);
+
+// every Nth Markdown element will be highlighted
+const NTH_MARKDOWN_HIGHLIGHT = 5;
+
 export const RangeField = ({
   thumbColorToken = 'white600',
-  trackColorToken = 'transparent',
+  trackColorToken = 'primary500',
   segmentHighlightColorToken = 'primary500',
   segmentColorToken = 'primary800',
   trackHeight = 10,
@@ -27,33 +44,50 @@ export const RangeField = ({
   value,
   step = 5,
 }: RangeFieldProps) => {
-  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    typeof onChange === 'function' && onChange(parseInt(event.target.value, 10));
+  const handleOnChange = (reactSliderValue: number | readonly number[]) => {
+    if (
+      typeof reactSliderValue !== 'number' ||
+      typeof onChange !== 'function' ||
+      isNaN(reactSliderValue)
+    ) {
+      return;
+    }
+    onChange(reactSliderValue);
   };
 
   return (
     <BoxContainer data-testid="RangeField-BoxContainer" trackColorToken={trackColorToken}>
-      <StyledInput
-        max={100}
-        min={0}
-        step={5}
-        thumbColorToken={thumbColorToken}
-        thumbHeight={thumbHeight}
-        trackHeight={trackHeight}
-        type="range"
+      <StyledSlider
+        height={6}
+        marks={5}
+        renderMark={(props) => {
+          const keyAsNumber = !props.key
+            ? 0
+            : typeof props.key === 'number' || typeof props.key === 'bigint'
+            ? Number(props.key)
+            : parseInt(props.key, 10);
+          if (keyAsNumber === 0) {
+            return null;
+          }
+          const isHighlighted = keyAsNumber % (step * NTH_MARKDOWN_HIGHLIGHT) === 0;
+          return (
+            <StyledMark
+              {...(props as object)}
+              height={6}
+              colorToken={isHighlighted ? 'white100' : 'black100'}
+            />
+          );
+        }}
+        step={step}
         value={value}
         onChange={handleOnChange}
+        max={100}
+        // TODO: nasty hack to get it working
+        renderTrack={Track as never}
+        min={0}
+        // TODO: nasty hack to get it working
+        renderThumb={Thumb as never}
       />
-      <SegmentBox>
-        {Array.from({ length: 100 / step }, (_, index) => (
-          <Segment
-            key={index}
-            colorToken={(index + 1) % step === 0 ? segmentHighlightColorToken : segmentColorToken}
-            data-testid={`RangeField-Segment-${index}`}
-            height={thumbHeight}
-          />
-        ))}
-      </SegmentBox>
     </BoxContainer>
   );
 };
