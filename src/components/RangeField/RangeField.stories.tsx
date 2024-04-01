503c747514350cb3c930805c89f38cee79f526de
ssbarbee <ssbarbee@users.noreply.github.com>
2024-04-01 18:12:25 +0200

    fix: improvements over step vs mark

--- src/components/RangeField/RangeField.stories.tsx
+++ src/components/RangeField/RangeField.stories.tsx
@@ -16,6 +16,53 @@ const Template: StoryFn<typeof RangeField> = (args) => {
 
 export const Default: StoryObj<typeof RangeField> = {
   args: {
+    value: 0,
+  },
+  render: Template,
+};
+
+export const WithThumbColor: StoryObj<typeof RangeField> = {
+  args: {
+    thumbColorToken: 'primary500',
+    value: 0,
+  },
+  render: Template,
+};
+
+export const WithTrackColor: StoryObj<typeof RangeField> = {
+  args: {
+    trackColorToken: 'black800',
+    value: 0,
+  },
+  render: Template,
+};
+
+export const WithMarkConfig: StoryObj<typeof RangeField> = {
+  args: {
+    highlightEveryNthMark: 2,
+    markColorToken: 'error500',
+    markHighlightColorToken: 'warning500',
+    step: 10,
+    value: 0,
+  },
+  render: Template,
+};
+
+export const WithSize: StoryObj<typeof RangeField> = {
+  args: {
+    markHeight: 10,
+    thumbColorToken: 'primary500',
+    thumbSize: 8,
+    trackColorToken: 'black800',
+    trackHeight: 15,
+    value: 0,
+  },
+  render: Template,
+};
+
+export const WithStep: StoryObj<typeof RangeField> = {
+  args: {
+    step: 4,
     thumbColorToken: 'primary500',
     trackColorToken: 'black800',
     value: 0,
