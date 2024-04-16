7822258a34f0e3a76229c73263ab4e4fa18d120e
Antonio <antoniosimeonov7@gmail.com>
2024-04-16 08:20:26 +0300

    fix: modal to close on ESC and onOutsideClick

--- src/components/Dialog/index.tsx
+++ src/components/Dialog/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { DialogBox } from './Dialog.styled';
 import { FloatingUIDialog, FloatingUIDialogContent } from './FloatingUIDialog';
@@ -8,11 +8,20 @@ type DialogProps = React.PropsWithChildren<{
 }>;
 
 export const Dialog: React.FunctionComponent<DialogProps> = ({ open, children }) => {
+  const [isOpen, setIsOpen] = useState(open);
+
+  useEffect(() => {
+    if (isOpen !== open) {
+      setIsOpen(open);
+    }
+  }, [open]);
+
   if (!children) {
     return null;
   }
+
   return (
-    <FloatingUIDialog open={open}>
+    <FloatingUIDialog open={isOpen} onOpenChange={setIsOpen}>
       <FloatingUIDialogContent>
         <DialogBox>{children}</DialogBox>
       </FloatingUIDialogContent>
