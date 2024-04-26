c7e68b0ba69026e1391a302856656eeb9dd40298
Antonio <antoniosimeonov7@gmail.com>
2024-04-26 16:18:24 +0300

    fix: linting, refactor: tests for transition

--- src/utils/create-transition/index.test.ts
+++ src/utils/create-transition/index.test.ts
@@ -3,17 +3,17 @@ import { createTransition } from './index';
 describe('createTransition utility function', () => {
   it('should return the default transition', () => {
     const result = createTransition();
-    expect(result).toBe('all 200ms ease-in');
+    expect(result).toBe('all 200ms linear');
   });
 
   it('should allow custom property with default duration and timing function', () => {
     const result = createTransition({ properties: 'opacity' });
-    expect(result).toBe('opacity 200ms ease-in');
+    expect(result).toBe('opacity 200ms linear');
   });
 
   it('should allow custom duration with default property and timing function', () => {
     const result = createTransition({ duration: 300 });
-    expect(result).toBe('all 300ms ease-in');
+    expect(result).toBe('all 300ms linear');
   });
 
   it('should allow custom timing function with default property and duration', () => {
@@ -23,7 +23,7 @@ describe('createTransition utility function', () => {
 
   it('should allow custom property and duration with default timing function', () => {
     const result = createTransition({ duration: 300, properties: 'opacity' });
-    expect(result).toBe('opacity 300ms ease-in');
+    expect(result).toBe('opacity 300ms linear');
   });
 
   it('should allow custom timing function, custom duration and custom properties', () => {
