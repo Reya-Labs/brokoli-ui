c4aedacbdef45125aeb343f19bdfecb842b74d15
Antonio <antoniosimeonov7@gmail.com>
2024-05-15 12:46:01 +0300

    fix: overlapping option buttons

--- src/components/OptionButtons/OptionButtons.styled.tsx
+++ src/components/OptionButtons/OptionButtons.styled.tsx
@@ -13,10 +13,6 @@ export const OptionButtonsBox = styled('div')`
   display: flex;
   flex-direction: row;
   position: relative;
-
-  > *:not(:first-child) {
-    margin-left: -16px;
-  }
 `;
 
 export const OptionButton = styled(Button, shouldNotForwardProps(['isActive']))<{
