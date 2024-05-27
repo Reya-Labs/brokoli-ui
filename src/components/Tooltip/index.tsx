f415d556f1d62f5617f817758c60ee365d554123
ssbarbee <ssbarbee@users.noreply.github.com>
2024-05-27 15:52:45 +0200

    feat: add tooltip color and typography options

--- src/components/Tooltip/index.tsx
+++ src/components/Tooltip/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { ColorTokens } from '../../foundation/Colors';
+import { TypographyTokens } from '../../foundation/Typography';
 import { Typography } from '../Typography';
 import {
   FloatingUITooltip,
@@ -11,13 +13,17 @@ import { TooltipBox, TriggerBox } from './Tooltip.styled';
 export type TooltipProps = {
   children: string | React.ReactNode;
   className?: string;
+  colorToken?: ColorTokens;
   trigger: string | React.ReactNode;
+  typographyToken?: TypographyTokens;
 };
 
 export const Tooltip: React.FunctionComponent<TooltipProps> = ({
   className,
   trigger,
   children,
+  typographyToken = 'bodySmallMedium',
+  colorToken = 'white100',
 }) => (
   <FloatingUITooltip>
     <FloatingUITooltipTrigger className={className}>
@@ -25,7 +31,7 @@ export const Tooltip: React.FunctionComponent<TooltipProps> = ({
     </FloatingUITooltipTrigger>
     <FloatingUITooltipContent>
       <TooltipBox limitWidth={typeof children === 'string'}>
-        <Typography colorToken="white100" typographyToken="bodySmallMedium">
+        <Typography colorToken={colorToken} typographyToken={typographyToken}>
           {children}
         </Typography>
       </TooltipBox>
