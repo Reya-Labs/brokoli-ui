ba9bef7583fe58b619c5b66b68ddeecb99a53b3f
ssbarbee <ssbarbee@users.noreply.github.com>
2024-06-26 11:44:03 +0200

    fix: icon

--- src/components/Icons/MarketIcon/index.tsx
+++ src/components/Icons/MarketIcon/index.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 
 import { Icon, IconProps } from './Icon';
-import { MarketIconStyled } from './MarketIcon.styled';
 
 export type MarketIconProps = IconProps & {
   size?: number;
 };
 
 export const MarketIcon: React.FunctionComponent<MarketIconProps> = ({ size, ...props }) => {
-  if (!size) {
-    return <Icon {...props} />;
-  }
-  return <MarketIconStyled size={size} {...props} />;
+  return <Icon {...props} height={size} width={size} />;
 };
--- src/components/Icons/TokenIcon/index.tsx
+++ src/components/Icons/TokenIcon/index.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 
 import { Icon, IconProps } from './Icon';
-import { TokenIconStyled } from './TokenIcon.styled';
 
 export type TokenIconProps = IconProps & {
   size?: number;
 };
 
 export const TokenIcon: React.FunctionComponent<TokenIconProps> = ({ size, ...props }) => {
-  if (!size) {
-    return <Icon {...props} />;
-  }
-  return <TokenIconStyled size={size} {...props} />;
+  return <Icon {...props} height={size} width={size} />;
 };
